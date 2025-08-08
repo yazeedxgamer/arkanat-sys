9081c5b4580d808769ad72eb0c6c06fd1fa5a289
yazeedxgamer <hihihihihi569@gmail.com>
2025-08-08 11:04:50 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-// Version: 1.0.7
+// Version: 1.0.8
 // === firebase-messaging-sw.js ===
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
